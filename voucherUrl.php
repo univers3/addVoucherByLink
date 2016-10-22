c6cd744ca1866b5c83c703c55d9367d1adc474a2
Damiano <damiano@MacBook-Air-di-Damiano.local>
2016-10-22 20:52:46 +0200

    First commit

---
+++ voucherUrl.php
@@ -0,0 +1,225 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class VoucherUrl extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'voucherUrl';
+        $this->tab = 'advertising_marketing';
+        $this->version = '1.0.0';
+        $this->author = 'Damiano Rodriguez';
+        $this->need_instance = 0;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Voucher Url Module');
+        $this->description = $this->l('Aggiungi un Voucher tramite un URL speciale');
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        Configuration::updateValue('VOUCHERURL_LIVE_MODE', false);
+        if (!parent::install() OR
+            !$this->registerHook('header') OR
+            !$this->registerHook('cart')
+        )
+            return false;
+        return true;
+    }
+
+    public function uninstall()
+    {
+        Configuration::deleteByName('VOUCHERURL_LIVE_MODE');
+
+        return parent::uninstall();
+    }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        /**
+         * If values have been submitted in the form, process.
+         */
+        if (((bool)Tools::isSubmit('submitVoucherUrlModule')) == true) {
+            $this->postProcess();
+        }
+
+        $this->context->smarty->assign('module_dir', $this->_path);
+
+        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
+
+        return $output.$this->renderForm();
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->table = $this->table;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitVoucherUrlModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'fields_value' => $this->getConfigFormValues(), /* Add values for your inputs */
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                'title' => $this->l('Settings'),
+                'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Live mode'),
+                        'name' => 'VOUCHERURL_LIVE_MODE',
+                        'is_bool' => true,
+                        'desc' => $this->l('Use this module in live mode'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Enabled')
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('Disabled')
+                            )
+                        ),
+                    ),
+                    array(
+                        'col' => 3,
+                        'type' => 'text',
+                        'prefix' => '<i class="icon icon-envelope"></i>',
+                        'desc' => $this->l('Enter a valid email address'),
+                        'name' => 'VOUCHERURL_ACCOUNT_EMAIL',
+                        'label' => $this->l('Email'),
+                    ),
+                    array(
+                        'type' => 'password',
+                        'name' => 'VOUCHERURL_ACCOUNT_PASSWORD',
+                        'label' => $this->l('Password'),
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+        return array(
+            'VOUCHERURL_LIVE_MODE' => Configuration::get('VOUCHERURL_LIVE_MODE', true),
+            'VOUCHERURL_ACCOUNT_EMAIL' => Configuration::get('VOUCHERURL_ACCOUNT_EMAIL', 'contact@prestashop.com'),
+            'VOUCHERURL_ACCOUNT_PASSWORD' => Configuration::get('VOUCHERURL_ACCOUNT_PASSWORD', null),
+        );
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+        $form_values = $this->getConfigFormValues();
+
+        foreach (array_keys($form_values) as $key) {
+            Configuration::updateValue($key, Tools::getValue($key));
+        }
+    }
+
+    /**
+    * Add the CSS & JavaScript files you want to be loaded in the BO.
+    */
+    public function hookBackOfficeHeader()
+    {
+        if (Tools::getValue('module_name') == $this->name) {
+            $this->context->controller->addJS($this->_path.'views/js/back.js');
+            $this->context->controller->addCSS($this->_path.'views/css/back.css');
+        }
+    }
+
+    /**
+     * Add the CSS & JavaScript files you want to be added on the FO.
+     */
+    public function hookHeader()
+    {
+        $this->context->controller->addJS($this->_path.'/views/js/front.js');
+        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
+        //global $cookie;
+        if (Tools::getValue('voucher')){
+            setcookie("voucherCode", Tools::getValue('voucher'));
+            //$cookie->voucherCode = Tools::getValue('voucher');
+        }
+    }
+
+    public function hookCart($params){
+
+        if (isset($_COOKIE['voucherCode'])){
+
+            $vouchersInCart = $params['cart']->getDiscounts();
+            $vouchersInCartIds = array();
+            if (count($vouchersInCart)>0){
+                foreach($vouchersInCart as $voucher){
+                    $vouchersInCartIds[] = $voucher['id_discount'];
+                }
+            }
+            $idDiscount = Discount::getIdByName($_COOKIE['voucherCode']);
+
+            if (in_array($idDiscount,$vouchersInCartIds)){
+                unset($_COOKIE['voucherCode']);
+            }
+            else{
+                $discountObj = New Discount($idDiscount);
+                $totalType = Cart::ONLY_PRODUCTS_WITHOUT_SHIPPING;
+                if ($params['cart']->getOrderTotal(true,$totalType) >= Configuration::get(PS_PURCHASE_MINIMUM) && $params['cart']->getOrderTotal(true,$totalType) >= $discountObj->minimal){
+                        $params['cart']->addDiscount($idDiscount);
+                        unset($params['cookie']->voucherCode);
+                }
+            }
+        }
+    }
+}
